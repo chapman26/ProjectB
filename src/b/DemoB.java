56da4a7e8048022417fe973e65624b40e68769bd
samchapman <samchapman@192.168.1.245>
2024-01-07 23:56:46 -0500

    123abc

---
+++ src/b/DemoB.java
@@ -0,0 +1,14 @@
+package b;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello again B");
+		
+		System.out.println("Sprint 1 work...");
+		
+		System.out.println("Sprint 2 work...");
+	}
+
+}
